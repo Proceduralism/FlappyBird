14ff6d47166f9a123d677e277b5ec4b297b683e4
Jihyun Nam <initialfx@gmail.com>
2020-11-06 16:27:53 -0800

    First Commit

---
+++ PyGame/py_game.py
@@ -0,0 +1,19 @@
+# pygame flappybird
+
+import pygame, time
+
+pygame.init()
+
+screen = pygame.display.set_mode((300, 600))
+
+while True:      
+   pygame.display.update()
+
+    time.sleep(3)
+    pygame.quit()
+
+
+
+
+
+
---
+++ PyGame/pyzero_game.py
@@ -0,0 +1,9 @@
+#PyGame Zero Flappy Bird
+
+TITLE = "My Flappy Bird"
+
+WIDTH = 300
+HEIGHT = 600
+
+def draw():
+    screen.fill((0, 0, 255))
