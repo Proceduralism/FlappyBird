7008ef39dbb3ea43cab1718abc954dc4e65a8fe6
Jihyun Nam <initialfx@gmail.com>
2020-11-07 11:38:29 -0800

    quick update

--- PyGame/py_game.py
+++ PyGame/py_game.py
@@ -1,19 +1,40 @@
-# pygame flappybird
+# Flappy Bird Pygame version
+# Original Code from Clear Code https://youtu.be/UZg49z76cLw
+# Modified by Jihyun Nam
 
-import pygame, time
+import pygame, sys
 
 pygame.init()
+pygame.display.set_caption('Flappy Bird')
 
-screen = pygame.display.set_mode((300, 600))
+screen = pygame.display.set_mode((288, 512)) # Create screen object
+clock = pygame.time.Clock() # Create clock object
 
-while True:      
-   pygame.display.update()
+bg_surface = pygame.image.load('sprites/background_day.png') # Load background file
+bg_surface = pygame.transform.scale2x(bg_surface) # 2x times scale up
 
-    time.sleep(3)
-    pygame.quit()
+ground_surface = pygame.image.load('sprites/ground.png') # Load background file
+ground_surface = pygame.transform.scale2x(ground_surface) # 2x times scale up
 
+ground_xpos = 0
 
+while True:
+    # Eveny handler      
+    for event in pygame.event.get():    # Getting every single event 
+            if event.type == pygame.QUIT:   
+                pygame.quit()   # End Game
+                sys.exit()  # For safe end program
 
+    # Drawing background 
+    screen.blit(bg_surface, (0, 0))
 
+    # Scrolling ground
+    ground_xpos -= 0.5    
+    screen.blit(ground_surface, (ground_xpos, 450))
+    screen.blit(ground_surface, (ground_xpos + 288, 450))
+    if(ground_xpos < -288):
+        ground_xpos = 0
 
-
+    # Update display
+    pygame.display.update()
+    clock.tick(120)
--- PyGame/pyzero_game.py
+++ PyGame/pyzero_game.py
@@ -1,9 +1,30 @@
-#PyGame Zero Flappy Bird
+# Flappy Bird Pygame Zero version
+
 
 TITLE = "My Flappy Bird"
+WIDTH = 400
+HEIGHT = 708
 
-WIDTH = 300
-HEIGHT = 600
+
 
 def draw():
-    screen.fill((0, 0, 255))
+    #screen.fill((0, 0, 0))
+    background1.draw()
+    background2.draw()
+   
+    #bird.draw()
+
+def update():        
+    background1.left -= 1
+    if background1.left < -400:
+        background1.left = 400
+
+    background2.left -= 1
+    if background2.left < -400:
+        background2.left = 400
+
+#bird = Actor('bird1', (100, 100))
+
+background1 = Actor('background', (200, 354))
+background2 = Actor('background', (200 + 400, 354))
+
