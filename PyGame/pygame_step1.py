71cbe45e0aaddd30e093e0234b039b1025cc1389
Jihyun Nam <initialfx@gmail.com>
2020-11-13 18:51:25 -0800

    pygame_step1.py end file

--- PyGame/pygame_step1.py
+++ PyGame/pygame_step1.py
@@ -10,6 +10,12 @@ pygame.display.set_caption('Flappy Bird')
 screen = pygame.display.set_mode((400, 708)) # Create screen object
 
 
+
+bg_surface1 = pygame.image.load('images/background.png')
+bg_surface2 = pygame.image.load('images/background.png')
+
+bg_pos = 0 # bg origin position
+
 while True:
     # Eveny handler      
     for event in pygame.event.get():    # Getting every single event 
@@ -17,4 +23,14 @@ while True:
                 pygame.quit()   # End Game
                 sys.exit()  # For safe end program
 
+    bg_pos -= 1 # move 1 pixel every tick
+
+    screen.blit(bg_surface1, (bg_pos, 0))
+    screen.blit(bg_surface2, (bg_pos + 400, 0))
+
+    if(bg_pos < -400):
+        bg_pos = 0
+
     pygame.display.update()
+
+    pygame.time.Clock().tick(120)
